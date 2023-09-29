59f20a5b4ba7b4724808f9df8f9093fba2b46959
Emily Wallace - Price <60661797+emilywallace-price@users.noreply.github.com>
2023-09-29 13:42:23 +0100

    github repo

--- resources/js/Components/Staff.vue
+++ resources/js/Components/Staff.vue
@@ -6,59 +6,77 @@
                     Our Staff
                 </h3>
                 <p class="text-gray-600 mb-8">
-                    We have a wonderful team of helpers here at Waterside Home Help Limited. All of our helpers are fully trained, insured, and hold enhanced DBS checks.
+                    We have a wonderful team of helpers here at Waterside Home Help Limited. All of our helpers are  fully trained, insured and hold enhanced DBS checks.
                     <br />
-                    Our staff all wear uniforms, which we find ensures they are easily recognizable to our clients.
+                    Our staff all wear uniforms, which we find ensures they are easily recognisable to our clients.
+
                 </p>
             </div>
             <Line></Line>
+
             <div class="flex flex-wrap">
-                <div v-for="member in members" :key="member.id" class="w-full sm:w-1/2 p-6">
+                <div class="w-full sm:w-1/2 p-6">
                     <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">
-                        {{ member.name }}
+                        Ruth
                     </h3>
                     <div class="w-full sm:w-1/2 p-6 rounded-lg float-right">
-                        <img class="rounded-t-lg sm:h-52 lg:h-80 xl:h-96 w-full mt-3" v-bind:src="'/images/' + member.image" alt="">
+                        <img class="rounded-lg" src="../Pages/images/Ruth.jpg" alt="">
                     </div>
-                    <p class="text-gray-600 mb-4" v-html="member.description">
+                    <p class="text-gray-600 mb-4">
+                        I am a mum to four children. 3 girls and a little boy. I’ve lived in the Waterside all of my life and have spent many years caring for people. My most recent job was as as a Senior Care Lead in a nursing home. Here I quickly realised how easily an elderly person can lose their independence due to age and illness. I've spent a lifetime looking after others, it's what I do best and what I am passionate about so being able to support people to continue to live their lives the way they want to, in their own homes is what makes me happy.
+                        The pandemic changed the way we were able to care for our aging relatives and I realised there was a 'gap' in the care sector. Many people are able to cope independently with their personal care needs but need that extra bit of assistance with things like housework, changing bed linen, preparing meals, transport to appointments, shopping or just spending time playing games or chatting over a cup of tea.
+                        So in March 2021 Waterside Home Help was born. Now 2 years on we have an amazing team of Helpers assisting our clients throughout the Waterside. We are a close team and I couldn't be prouder of how far we have come. It is abundantly apparent in the testimonials that our helpers make a massive difference to their clients lives and reading the information given by the helpers it is also apparent the affect the clients have on us.
                     </p>
                 </div>
+<!--                <div class="w-5/6 sm:w-1/2 p-6">-->
+<!--                    <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">-->
+<!--                        Wendy-->
+<!--                    </h3>-->
+<!--                    <div class="w-full sm:w-1/2 p-6 rounded-lg float-right">-->
+<!--                        <img class="rounded-lg" src="../Pages/images/staff2.jpeg" alt="">-->
+<!--                    </div>-->
+<!--                    <p class="text-gray-600 mb-4">-->
+<!--                        Details about Wendy coming soon ....-->
+<!--                    </p>-->
+<!--                </div>-->
+<!--                <Line></Line>-->
+<!--                <div class="w-full sm:w-1/2 p-6">-->
+<!--                    <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">-->
+<!--                        Liz-->
+<!--                    </h3>-->
+<!--                    <div class="w-full sm:w-1/2 p-6 rounded-lg float-right">-->
+<!--                        <img class="rounded-lg" src="../Pages/images/liz.jpeg" alt="">-->
+<!--                    </div>-->
+<!--                    <p class="text-gray-600 mb-4">-->
+<!--                        Details about Liz coming soon ....-->
+<!--                    </p>-->
+<!--                </div>-->
+<!--                <div class="w-full sm:w-1/2 p-6">-->
+<!--                    <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">-->
+<!--                        Lara-->
+<!--                    </h3>-->
+<!--                    <div class="w-full sm:w-1/2 p-6 rounded-lg float-right">-->
+<!--                        <img class="rounded-lg" src="../Pages/images/staff1.jpeg" alt="">-->
+<!--                    </div>-->
+<!--                    <p class="text-gray-600 mb-4">-->
+<!--                        Details about Lara coming soon ....-->
+<!--                    </p>-->
+<!--                </div>-->
             </div>
-            <Line></Line>
         </div>
     </section>
 </template>
 
 <script>
 import Line from "./Line";
-import axios from "axios";
 export default {
     name: "Staff",
-    components: { Line },
-    data() {
-        return {
-            members: null,
-        };
-    },
-    created() {
-        axios.get(`/api/staff-members`)
-            .then(response => {
-                console.log(response.data);
-                this.members = response.data;
-            })
-            .catch(e => {
-                this.errors.push(e);
-            });
-    },
-    methods: {
-        getImageUrl(filename) {
-            return process.env.BASE_URL + 'images/' + filename;
-        }
-    }
-};
+    components: { Line}
+}
 </script>
 
 <style scoped>
+
 .ruth {
     top: -1rem;
     /*left: -1.3rem;*/
@@ -75,3 +93,4 @@ export default {
     position: relative;
 }
 </style>
+
--- resources/js/Components/Updates.vue
+++ resources/js/Components/Updates.vue
@@ -11,7 +11,7 @@
                     <ul role="list">
                         <li class="mb-4 m-auto" v-for="update in updates" v-bind:key="update.id">
                             <div @click="toggleItem(update)" class="m-auto w-5/6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                                <img class="rounded-t-lg sm:h-52 lg:h-80 xl:h-96 w-full mt-3" v-bind:src="'/images/' + update.image" alt="">
+                                <img class="rounded-t-lg sm:h-52 lg:h-80 mx-auto mt-3 object-center" v-bind:src="'/images/' + update.image" alt="">
                                 <div class="icon">
                                     <svg v-if="!update.isVisible" class="w-6 h-6 dark:text-white m-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"></path></svg>
                                     <svg v-if="update.isVisible" class="w-6 h-6 dark:text-white m-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z"></path></svg>
