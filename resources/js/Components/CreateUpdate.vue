50cd83a930d4cfd9f5f9713c545885141a673b75
Emily Wallace - Price <60661797+emilywallace-price@users.noreply.github.com>
2023-10-23 18:42:55 +0100

    update validation

--- resources/js/Components/CreateUpdate.vue
+++ resources/js/Components/CreateUpdate.vue
@@ -3,6 +3,15 @@
         <div class="w-4/5 p-4 align-middle">
             <h1 class="text-xl text-blue-800 text-center font-bold">Create an Update</h1>
             <br>
+            <div v-if="error" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">Holy smokes!</strong>
+                <br>
+                <span class="block sm:inline">{{ error }}</span>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+              </span>
+            </div>
+            <br>
             <form enctype="multipart/form-data">
                 <label class="text-xl text-blue-800">Title:</label>
                 <input
@@ -71,6 +80,7 @@ export default {
             content: "",
             image: null,
             updates:null,
+            error: null,
         }
     },
     methods: {
@@ -149,7 +159,21 @@ export default {
                     if (this.image) {
                         this.image = null;
                     }
+                })
+                .catch((error) => {
+                    console.log(error);
+                    if (error.response && error.response.data) {
+                        let errorMessage = error.response.data.message;
+                        if (errorMessage === 'Undefined variable $image_new_name') {
+                            this.error = 'Please add a image';
+                        } else {
+                            this.error = 'Please add content';
+                        }
+                    } else {
+                        this.error = 'An error occurred.';
+                    }
                 });
+
         },
         onImageChange(e){
             console.log(e.target.files[0]);
